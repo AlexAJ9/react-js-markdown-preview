71406fd24443ef5ec535470f68002d7ca714fa77
Alex <a4319@mail.bg>
2019-11-16 23:31:41 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import marked  from 'marked';
+import marked from 'marked';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -6,76 +6,64 @@ import ReactDOM from "react-dom";
 
 
 
-  const textarea={
+const textarea = {
     width: "100%",
-    backgroundColor:"black",
-    color:"white",
-  height: window.innerHeight, 
-  border:"none",
-  overflow:"hidden"
-  };
-  const preview={
-    height: window.innerHeight ,
+    backgroundColor: "black",
+    color: "white",
+    height: window.innerHeight,
+    border: "none",
+    overflow: "hidden"
+};
+const preview = {
+    height: window.innerHeight,
     overflow: "scroll",
-    backgroundColor:"black",
-    color:"white"
-  }
-  marked.setOptions({
+    backgroundColor: "black",
+    color: "white"
+}
+marked.setOptions({
     breaks: true
-  });
-  const renderMarkdown = new marked.Renderer();
+});
 
-  class Preview extends  React.Component{
-  constructor(props)
-    {
-      super(props)
-      this.state={
-        text:""
-  }
+const renderMarkdown = new marked.Renderer();
+class Preview extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            text: ""
+        }
     }
-    
-    render(){
-      
-      return <div class ="row"style={preview}  dangerouslySetInnerHTML={{ __html: marked(this.props.text, { renderer: renderMarkdown }) }} ></div>;
-      
-  
+    render() {
+        return <div class="row" style={preview} dangerouslySetInnerHTML={{ __html: marked(this.props.text, { renderer: renderMarkdown }) }} ></div>;
     }
-
-                                                                      
-  }
-  class App extends React.Component {
-    constructor(props){
-      super(props);
-      this.state={
-        input:`# Write some Markdown here :)
-  `,
-          text:[]
-      }
+}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: `# Hello :)`,
+            text: []
+        }
     }
-    handleChange(event){
-      this.setState({
-            input:event.target.value    
-      })
-  }
-  
-    render(){
-      
-      return (
-      
-        
-      <div class="flex-box">
-          
-         <div class="row"> <textarea style={textarea}  onChange={this.handleChange.bind(this)} value={this.state.input} ></textarea></div>
-          <Preview text={this.state.input} />
-          
-        </div>
-      );
-        
-      
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        })
     }
-  }
+    render() {
+        return (
+            <div class="flex-box" >
+                <div class="row" > < textarea style={textarea}
+                    onChange={this.handleChange.bind(this)}
+                    value={this.state.input} > </textarea></div >
+                <Preview text={this.state.input} />
+            </div>
+        );
 
 
- 
+    }
+}
+
+
+
 
 export default App;
